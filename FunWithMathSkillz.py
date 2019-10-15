44004435769b240e103c65c6eb721b25cec66591
100134315 <100134315@lubbockisd.net>
2019-10-15 15:12:58 -0500

    commit of 10/15

--- FunWithMathSkillz.py
+++ FunWithMathSkillz.py
@@ -8,3 +8,30 @@ import math
 
 print(9801 / (2206 * math.sqrt(2)))
 
+def archimedes(numSides):
+    innerAngelB = 360.0 / numSides
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(archimedes(4))
+print(archimedes(8))
+print(archimedes(16))
+
+
+for sides in range(8, 10, 8):
+    print(sides, archimedes(sides))
+
+# see the loop above. in addition to the value of pi, print he difference
+# between the values calculated by the archimedes function and by math.pi.
+
+
